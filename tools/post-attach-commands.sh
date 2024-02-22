90c8effb4b950e7fed3ccec6a64759deaab7f18e
Marcin Lewandowski <mlewand69@gmail.com>
2024-02-22 22:07:49 +0100

    [

--- tools/post-attach-commands.sh
+++ tools/post-attach-commands.sh
@@ -10,12 +10,5 @@ pip install openai --upgrade
 pip install ollama --upgrade
 pip install litellm --upgrade
 
-echo "===== INSTALL RC FILE ..."
-[ ! -e ~/.neophyterc ] && cp .neophyterc ~/.neophyterc && echo "- RC FILE COPIED TO ~/.neophyterc"
-grep --quiet ". ~/.neophyterc" < ~/.bashrc || echo "
-# NEOPHYTE Tutorial environment update
-. ~/.neophyterc
-" >> ~/.bashrc
-
 echo ""
 echo "READY :-)"
--- tools/post-create-commands.sh
+++ tools/post-create-commands.sh
@@ -2,4 +2,10 @@
 # NEOPHYTE Tutorial by mlewand69@gmail.com
 # postCreateCommand"
 
-#code --install-extension ms-python.python
+echo "===== INSTALL RC FILE ..."
+[ ! -e ~/.neophyterc ] && cp .neophyterc ~/.neophyterc && echo "- RC FILE COPIED TO ~/.neophyterc"
+grep --quiet ". ~/.neophyterc" < ~/.bashrc || echo "
+# NEOPHYTE Tutorial environment update
+export PATH=$PATH:/home/codespace/tools
+. ~/.neophyterc
+" >> ~/.bashrc
