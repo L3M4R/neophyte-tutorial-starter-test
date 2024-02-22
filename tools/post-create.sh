c804975ed0ccf5b403f16eff6ea20ac5da028702
Marcin Lewandowski <mlewand69@gmail.com>
2024-02-22 22:51:18 +0100

    f

--- tools/post-create.sh
+++ tools/post-create.sh
@@ -2,6 +2,8 @@
 # NEOPHYTE Tutorial by mlewand69@gmail.com
 # postCreateCommand"
 
+touch neophyte.creating
+
 echo "===== INSTALL ENVIRONMENT FILE ..."
 [ ! -e ~/.neophyterc ] && cp .neophyterc ~/.neophyterc && echo "- RC FILE COPIED TO ~/.neophyterc"
 grep --quiet ". ~/.neophyterc" < ~/.bashrc || echo "
