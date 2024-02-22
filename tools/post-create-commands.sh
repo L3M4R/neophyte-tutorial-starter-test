5df0ddeafd125061f4048e672c7064fb5c074765
Marcin Lewandowski <mlewand69@gmail.com>
2024-02-22 22:05:52 +0100

    gjk

--- tools/post-create-commands.sh
+++ tools/post-create-commands.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 # NEOPHYTE Tutorial by mlewand69@gmail.com
-# postAttachCommand
+# postCreateCommand"
 
-echo "===== INSTALL EXTENSIONS ..."
-code --install-extension ms-python.python
-
-echo "===== INSTALL MODULES ..."
-pip install openai --upgrade
-pip install ollama --upgrade
-pip install litellm --upgrade
-
-echo "===== INSTALL RC FILE ..."
-[ ! -e ~/.neophyterc ] && cp .neophyterc ~/.neophyterc && echo "- RC FILE COPIED TO ~/.neophyterc"
-grep --quiet ". ~/.neophyterc" < ~/.bashrc || echo "
-# NEOPHYTE Tutorial environment update
-. ~/.neophyterc
-" >> ~/.bashrc && echo "- RC ADDED TO ~/.bashrc"
-
-echo "READY :-)"
+#code --install-extension ms-python.python
