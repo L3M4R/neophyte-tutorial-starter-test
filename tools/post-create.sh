55015ead7fb4dc5def2531d463882c2c36f95c30
Marcin Lewandowski <mlewand69@gmail.com>
2024-02-22 23:19:17 +0100

    j

--- tools/post-create.sh
+++ tools/post-create.sh
@@ -2,16 +2,15 @@
 # NEOPHYTE Tutorial by mlewand69@gmail.com
 # postCreateCommand (after container creation)
 
-echo "===== INSTALL ENVIRONMENT FILE ..."
-[ ! -e ~/.neophyterc ] && cp .neophyterc ~/.neophyterc && echo "- RC FILE COPIED TO ~/.neophyterc"
-grep --quiet ". ~/.neophyterc" < ~/.bashrc || echo "
-# NEOPHYTE Tutorial environment update
-. ~/.neophyterc
-" >> ~/.bashrc
-
 echo "===== INSTALL MODULES ..."
 pip install openai
 pip install ollama
 pip install litellm
 
-date >> neophyte.created
+echo "===== INSTALL ENVIRONMENT FILE ..."
+[ ! -e ~/.neophyterc ] && mv .neophyterc ~/.neophyterc && echo "- RC FILE MOVED TO ~/.neophyterc"
+grep --quiet ". ~/.neophyterc" < ~/.bashrc || echo "
+# NEOPHYTE Tutorial environment update
+. ~/.neophyterc
+" >> ~/.bashrc
+
