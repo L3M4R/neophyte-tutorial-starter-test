934f6c0769805ac60eaf2af68921f7979a7d4eae
Marcin Lewandowski <36675859+L3M4R@users.noreply.github.com>
2024-02-24 15:45:26 +0100

    Update post-create-test.sh

--- tools/post-create-test.sh
+++ tools/post-create-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # NEOPHYTE LLM Tutorial by mlewand69@gmail.com
 
-sleep 6
+sleep 12
 
 docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
 
@@ -10,7 +10,7 @@ sleep 3
 docker exec -it ollama /bin/bash -c "ollama pull gemma:2b"
 docker exec -it ollama /bin/bash -c "ollama list"
 
-sleep 6
+sleep 3
 
 tools/post-create-test.py
 
